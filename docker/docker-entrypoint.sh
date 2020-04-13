38c82a20114c39c9712c8ae2be698cdfaf1783da
Summer <summer.book@tapque.com>
2020-04-13 10:04:39 +0800

    redis 密码

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -85,7 +85,7 @@ do
             sed -i "/om.adc.domain/s@om.adc.domain.*@om.adc.domain: ${!env_var}@g" ${CONFILE}/application-loc.yml
             continue
         fi
-        if [[ ${item_name} = "redis_password" ]];then
+        if [[ ${item_name} = "redis-password" ]];then
             loginfo_note "[Configuring] ${item_name} in ${CONFILE}/application-loc.yml"
             sed -i "/app.redis.password/s@app.redis.password.*@app.redis.password: ${!env_var}@g" ${CONFILE}/application-loc.yml
             continue
